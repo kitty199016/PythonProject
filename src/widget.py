2666ed26aca4310e2a36c89d720800f5f9f1d5c2
Екатерина Калингер <kalinger90@list.ru>
2026-06-16 20:21:49 +0300

    fix linter error

--- src/widget.py
+++ src/widget.py
@@ -1,22 +1,23 @@
-from masks import get_mask_account
+from datetime import datetime
+from masks import get_mask_account, get_mask_card_number
 
-from masks import get_mask_card_number
 
-def mask_account_card(text:str) -> str | None:
+def mask_account_card(text: str) -> str | None:  # функция обрабатывает информацию о картах и счетах
     if "Счет" in text:
         for index, char in enumerate(text):
             if char.isdigit():
-                cipher = get_mask_account(int(text[index-1:]))
+                cipher = get_mask_account(int(text[index - 1:]))
                 return text[:index] + str(cipher)
         return None
     else:
         for index, char in enumerate(text):
             if char.isdigit():
-                cipher = get_mask_card_number(int(text[index-1:]))
+                cipher = get_mask_card_number(int(text[index - 1:]))
                 return text[:index] + str(cipher)
         return None
 
 
-
-a = mask_account_card("Visa Platinum 7000792289606361")
-print(a)
+def get_date(iso_string: str) -> str:  # функция перевода даты в формат дд.мм.гггг
+    dt_obj = datetime.fromisoformat(iso_string)
+    formatted_date_only = dt_obj.date().strftime("%d.%m.%Y")
+    return formatted_date_only
