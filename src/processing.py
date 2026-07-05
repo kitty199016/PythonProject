cd4cafc1538044ca2ecdffddeff5e94703d773f5
Екатерина Калингер <kalinger90@list.ru>
2026-07-05 16:31:00 +0300

    fix add anotation type

--- src/processing.py
+++ src/processing.py
@@ -1,7 +1,6 @@
-def filter_by_state(cards:list[dict], state = 'EXECUTED') -> list[dict]:
+def filter_by_state(cards: list[dict], state: str = 'EXECUTED') -> list[dict]:
      """Функция принимает список словарей и опционально значение
      для ключа state (по умолчанию 'EXECUTED').
-
      :cards - входные данные(список словарей)
      :state - сортировка по статусу
      return - новый список словарей"""
@@ -9,13 +8,11 @@ def filter_by_state(cards:list[dict], state = 'EXECUTED') -> list[dict]:
      return result
 
 
-def sort_by_date(data_input:list[dict], directions=True) -> list[dict]:
+def sort_by_date(data_input:list[dict], directions: bool = True) -> list[dict]:
      """Функция сортирует словари по дате
-
      :data_input -входные данные(список словарей)
      :directions - параметр сортировки
      :return - отсортированнфй список словарей
      """
      data_sort = sorted(data_input, key=lambda x: x["date"], reverse = directions)
      return data_sort
-a = filter_by_state()
