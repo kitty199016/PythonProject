0c11342101807f2d45cff0a05b1c3bbbbac5fd9d
Екатерина Калингер <kalinger90@list.ru>
2026-07-05 16:44:55 +0300

    fix linter error

--- src/processing.py
+++ src/processing.py
@@ -1,18 +1,17 @@
-def filter_by_state(cards: list[dict], state: str = 'EXECUTED') -> list[dict]:
-     """Функция принимает список словарей и опционально значение
-     для ключа state (по умолчанию 'EXECUTED').
-     :cards - входные данные(список словарей)
-     :state - сортировка по статусу
-     return - новый список словарей"""
-     result = [i for i in cards if i['state'] == state]
-     return result
+def filter_by_state(cards: list[dict], state: str = "EXECUTED") -> list[dict]:
+    """Функция принимает список словарей и опционально значение
+    для ключа state (по умолчанию 'EXECUTED').
+    :cards - входные данные(список словарей)
+    :state - сортировка по статусу
+    return - новый список словарей"""
+    result = [i for i in cards if i["state"] == state]
+    return result
 
 
-def sort_by_date(data_input:list[dict], directions: bool = True) -> list[dict]:
-     """Функция сортирует словари по дате
-     :data_input -входные данные(список словарей)
-     :directions - параметр сортировки
-     :return - отсортированнфй список словарей
-     """
-     data_sort = sorted(data_input, key=lambda x: x["date"], reverse = directions)
-     return data_sort
+def sort_by_date(data_input: list[dict], directions: bool = True) -> list[dict]:
+    """Функция сортирует словари по дате
+    :data_input -входные данные(список словарей)
+    :directions - параметр сортировки
+    :return - отсортированнфй список словарей"""
+    data_sort = sorted(data_input, key=lambda x: x["date"], reverse=directions)
+    return data_sort
