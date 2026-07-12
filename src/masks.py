7fb7db392159aea25d0fc041126762487c626c0b
Екатерина Калингер <kalinger90@list.ru>
2026-07-12 16:30:59 +0300

    add test_masks.py

--- src/masks.py
+++ src/masks.py
@@ -1,13 +1,20 @@
-def get_mask_card_number(number_card_input: int) -> str:  # функция редактирует номер карты по маске
-    number_card = str(number_card_input)
-    if len(number_card) != 16:
-        raise ValueError("Номер не верный")
-    else:
-        number_card_code = number_card[0:4] + " " + number_card[4:6] + "** **** " + number_card[12:]
-        return number_card_code
+def get_mask_card_number(number_card_input: int) -> str:
+    '''функция редактирует номер карты по маске
+    Visa Platinum 7000792289606361  # входной аргумент
+    Visa Platinum 7000 79** **** 6361  # выход функции'''
 
+    number_card = str(number_card_input)
+    if number_card.isdigit() and len(number_card) >= 12:
+        number_card_code = number_card[0:6] + ("*" * (len(number_card) - 10)) + number_card[-4:]
+        result = ' '.join(number_card_code[i:i + 4] for i in range(0, len(number_card_code), 4))
+        return result
+    else:
+        raise ValueError("Некорректный номер карты")
 
 def get_mask_account(account_number_input: int) -> str:  # функция редактирует номер счета по маске
     account_number = str(account_number_input)
-    account_number_code = "**" + account_number[-4:]
-    return account_number_code
+    if account_number.isdigit() and len(account_number) > 0:
+        account_number_code = "**" + account_number[-4:]
+        return account_number_code
+    else:
+        raise ValueError("Некорректный номер счета")
---
+++ tests/test_masks.py
@@ -0,0 +1,33 @@
+from src.masks import get_mask_account, get_mask_card_number
+
+import pytest
+
+cases = [(1111111111111111, "1111 11** **** 1111"),
+         (222222222222, "2222 22** 2222")]
+cases_negative = [(3333333,), ("",),("fff43432",)]
+
+@pytest.mark.parametrize("number_card_input, result", cases)
+def test_get_mask_card_number_positive(number_card_input:int, result:str):
+    assert get_mask_card_number(number_card_input) == result
+
+
+@pytest.mark.parametrize("number_card_input", cases_negative)
+def test_get_mask_card_number_negative(number_card_input:int):
+    with pytest.raises(ValueError):
+        get_mask_card_number(number_card_input)
+
+
+
+cases_account = [(123456789, "**6789"),(73654108430135874305, "**4305")]
+cases_account_negative = [(), ("fe4545432")]
+
+
+@pytest.mark.parametrize("account_number_input, result", cases_account)
+def test_get_mask_account_positive(account_number_input:int, result:str):
+    assert get_mask_account(account_number_input) == result
+
+
+@pytest.mark.parametrize("account_number_input", cases_account_negative)
+def test_gget_mask_account_negative(account_number_input:int):
+    with pytest.raises(ValueError):
+        get_mask_account(account_number_input)
