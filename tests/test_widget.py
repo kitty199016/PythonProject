1759f467fc9549f9aef3178a9aeab479adeb90be
Екатерина Калингер <kalinger90@list.ru>
2026-07-13 20:55:50 +0300

    fixed error mypy

--- tests/test_widget.py
+++ tests/test_widget.py
@@ -1,12 +1,11 @@
-
-from src.widget import mask_account_card, get_date
-
 import pytest
 
-from datetime import datetime
+from src.widget import get_date, mask_account_card
 
-cases_mask_account_card = [("Visa Platinum 7000792289606361", "Visa Platinum 7000 79** **** 6361"),
-         ("Счет 73654108430135874305", "Счет **4305")]
+cases_mask_account_card = {
+    ("Visa Platinum 7000792289606361", "Visa Platinum 7000 79** **** 6361"),
+    ("Счет 73654108430135874305", "Счет **4305"),
+}
 cases_mask_account_card_negative = [("3333333"), (""), ("fff43432")]
 
 
@@ -20,14 +19,17 @@ def test_get_mask_account_card_negative(text: str) -> None:
     with pytest.raises(ValueError):
         mask_account_card(text)
 
+
 cases_get_date = [("2024-03-11T02:26:18.671407", "11.03.2024")]
-cases_get_date_negative =[("Дата: 01.01.2021"),("32/13/2021")]
+cases_get_date_negative = [("Дата: 01.01.2021"), ("32/13/2021")]
+
 
 @pytest.mark.parametrize("iso_string, result", cases_get_date)
-def test_get_date_positive(iso_string:str, result:str) -> None:
+def test_get_date_positive(iso_string: str, result: str) -> None:
     assert get_date(iso_string) == result
 
+
 @pytest.mark.parametrize("iso_string", cases_get_date_negative)
-def test_get_date_negative(iso_string:str) -> None:
+def test_get_date_negative(iso_string: str) -> None:
     with pytest.raises(ValueError):
         mask_account_card(iso_string)
