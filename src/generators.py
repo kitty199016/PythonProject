3dc53650087f9c9d3dbdb2f7a53b7f4079f8c1d7
Екатерина Калингер <kalinger90@list.ru>
2026-07-29 20:04:30 +0300

    add card_number_generator

--- src/generators.py
+++ src/generators.py
@@ -19,3 +19,16 @@ def transaction_descriptions(transactions:dict) -> Generator[Any, Any, None]:
     """
     for transaction in transactions:
         yield transaction.get('description')
+
+
+def card_number_generator(start, end):
+    """
+    Генератор, который выдает номера карт в формате XXXX XXXX XXXX XXXX
+    в диапазоне от start до end включительно.
+    """
+    for number in range(start, end + 1):
+        card_str = f"{number:016d}"
+
+        formatted_card = f"{card_str[0:4]} {card_str[4:8]} {card_str[8:12]} {card_str[12:16]}"
+
+        yield formatted_card
