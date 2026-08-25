850d5d7136a81174ffea651ec7d0d7b4e93da739
Екатерина Калингер <kalinger90@list.ru>
2026-08-25 13:22:55 +0300

    add read_excel

--- src/table_utils.py
+++ src/table_utils.py
@@ -1,4 +1,5 @@
 import csv
+import pandas as pd
 from typing import List, Dict
 
 
@@ -27,3 +28,24 @@ def read_csv(file_path: str = "../data/transactions.csv") -> List[Dict[str, str]
         print(f"Произошла ошибка при чтении файла: {e}")
 
     return transactions
+
+
+def read_excel(file_path: str = "../data/transactions_excel.xlsx") -> List[Dict[str, any]]:
+    """Считывает финансовые операции из файла Excel.
+
+    Args:
+        file_path (str): Путь к файлу Excel (.xlsx).
+
+    Returns:
+        List[Dict[str, any]]: Список словарей с транзакциями.
+    """
+    try:
+        df = pd.read_excel(file_path)
+        df = df.where(pd.notnull(df), None)  # Заменяем пустые ячейки NaN на None
+        return df.to_dict(orient="records")
+    except FileNotFoundError:
+        print(f"Ошибка: Файл по пути '{file_path}' не найден.")
+        return []
+    except Exception as e:
+        print(f"Произошла ошибка при чтении файла: {e}")
+        return []
